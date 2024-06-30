8009ab03304da179d1a834a395db7dd036afd705
Gleb <glebmayorshin@gmail.com>
2024-06-30 14:33:51 +0500

    A byte Of Python addition

---
+++ Byte of Python/str_format.py
@@ -0,0 +1,7 @@
+name = 'Kek'
+age = '90'
+
+print('Age of {0} -- {1} years'.format(name, age))
+print('Why is {0} reading this code?'.format(name))
+print('Возраст ' + name + ' -- ' + str(age) + ' лет.')
+print('Age of {} -- {} years'.format(name, age))
